46dbdf419d577a544e470f5fab27a182dd74c880
aaadasdas <aaadasdas>
2022-05-13 13:49:53 +0200

    getTASKS

--- controllers/task.js
+++ controllers/task.js
@@ -1,16 +1,34 @@
 const Task = require ("../models/task");
 
-function createTask(req,res){
-    cons//t task = new Task()
-    const params = req.body
-    task.title = params.title
-    task.descripcion = params.description
+async function createTask(req,res){
+    const task = new Task();
+    const params = req.body;
+    task.title = params.title;
+    task.descripcion = params.description;
 
+    try{
+        const taskStore = task.save();
 
-    console.log("Creando nuestra primera tarea");
-    console.log(req.body);
+        if(!taskStore){
+            res.status(400).send({msg: "No se ha guardado la tarea"});
+
+        } else{
+            res.status(200).send({task: taskStore});
+        }
+    }catch(error){
+        res.status(500).send(error);
+    }
+
+    //console.log("Creando nuestra primera tarea");
+    //console.log(req.body);
+}
+
+function getTasks(req,res){
+    console.log("Obteniendo las tareas");
 }
 
 module.exports = {
     createTask,
+    getTasks,
+
 }
